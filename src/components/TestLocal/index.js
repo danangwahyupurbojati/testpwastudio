da96c5dfc1fcdb705274b4183faddc8efd563a27
danangwahyupurbojati <danang.wahyu@sirclo.com>
2022-03-16 16:29:24 +0700

    first commit

---
+++ src/components/TestLocal/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestLocal = () => {
+  return (
+    <div>
+        <h1>halo test dengan menggunakan local intercept</h1>
+    </div>
+  )
+}
+
+export default TestLocal;
